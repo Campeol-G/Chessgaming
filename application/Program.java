c8253326cee5f529f97b52df80e011d0f2706a0a
ChicoMagnusOpus <gustavocampeol28@gmail.com>
2026-07-11 15:51:06 -0300

    initial board and piece

--- application/Program.java
+++ application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Program {
   public static void main(String[] args) {
     Position p1 = new Position(1, 3);
     System.out.println(p1);
+    Board board = new Board(8, 8);
   }
 }
